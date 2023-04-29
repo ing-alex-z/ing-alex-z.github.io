ba00aa74fddc092f6df88980eb15ec13d8c02d6e
ing-alex-z <115185384+ing-alex-z@users.noreply.github.com>
2023-04-29 15:44:30 +0200

    Sostituito files

--- __settings__.js
+++ __settings__.js
@@ -1,6 +1,6 @@
 window.ASSET_PREFIX = "";
 window.SCRIPT_PREFIX = "";
-window.SCENE_PATH = "1732384.json";
+window.SCENE_PATH = "1732385.json";
 window.CONTEXT_OPTIONS = {
     'antialias': true,
     'alpha': false,
