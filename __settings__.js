629f250474e5e8b0aacb9d95c32630f0d31e59ec
ing-alex-z <115185384+ing-alex-z@users.noreply.github.com>
2023-04-30 15:27:10 +0200

    Reset R3

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1732385.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 131755735, 131756776, 131756798, 131756799, 131756800, 131770501, 131770522, 131770625, 131770676, 131771440, 131783607 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
