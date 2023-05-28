4bc6d6f6cf3cba4dafd7b10521e75942b13c84d8
ing-alex-z <115185384+ing-alex-z@users.noreply.github.com>
2023-05-28 23:06:28 +0200

    Aggiornamenti in test, redirect principale su 404

--- test/navbar.js
+++ test/navbar.js
@@ -9,15 +9,18 @@ var util = {
   },
   scrollEvent() {
     var scrollPosition = $(document).scrollTop();
-    
-    $.each(util.scrollMenuIds, function(i) {
-      var link = util.scrollMenuIds[i],
-          container = $(link).attr("href"),
-          containerOffset = $(container).offset().top,
-          containerHeight = $(container).outerHeight(),
-          containerBottom = containerOffset + containerHeight;
 
-      if (scrollPosition < containerBottom - 20 && scrollPosition >= containerOffset - 20) {
+    $.each(util.scrollMenuIds, function (i) {
+      var link = util.scrollMenuIds[i],
+        container = $(link).attr("href"),
+        containerOffset = $(container).offset().top,
+        containerHeight = $(container).outerHeight(),
+        containerBottom = containerOffset + containerHeight;
+
+      if (
+        scrollPosition < containerBottom - 20 &&
+        scrollPosition >= containerOffset - 20
+      ) {
         $(link).addClass("active");
       } else {
         $(link).removeClass("active");
@@ -26,11 +29,9 @@ var util = {
   }
 };
 
-$(document).ready(function() {
-  
+$(document).ready(function () {
   util.scrollMenuIds = $("a.nav-link[href]");
   $("#menu").click(util.mobileMenu);
   $(window).resize(util.windowResize);
   $(document).scroll(util.scrollEvent);
-  
 });
