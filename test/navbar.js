1b13e5f67b3926c90e9480b569696c6a39198165
ing-alex-z <115185384+ing-alex-z@users.noreply.github.com>
2023-06-04 21:53:11 +0200

    Complete overhaul of code

--- test/navbar.js
+++ test/navbar.js
@@ -1,37 +1,53 @@
-var util = {
-  mobileMenu() {
-    $("#nav").toggleClass("nav-visible");
-  },
-  windowResize() {
-    if ($(window).width() > 800) {
-      $("#nav").removeClass("nav-visible");
-    }
-  },
-  scrollEvent() {
-    var scrollPosition = $(document).scrollTop();
-
-    $.each(util.scrollMenuIds, function (i) {
-      var link = util.scrollMenuIds[i],
-        container = $(link).attr("href"),
-        containerOffset = $(container).offset().top,
-        containerHeight = $(container).outerHeight(),
-        containerBottom = containerOffset + containerHeight;
-
-      if (
-        scrollPosition < containerBottom - 20 &&
-        scrollPosition >= containerOffset - 20
-      ) {
-        $(link).addClass("active");
-      } else {
-        $(link).removeClass("active");
-      }
-    });
-  }
-};
-
 $(document).ready(function () {
+  var util = {
+    mobileMenu() {
+      $("#nav").toggleClass("nav-visible");
+    },
+    windowResize() {
+      if ($(window).width() > 800) {
+        $("#nav").removeClass("nav-visible");
+      }
+    },
+    scrollEvent() {
+      var scrollPosition = $(document).scrollTop();
+
+      $.each(util.scrollMenuIds, function (i) {
+        var link = util.scrollMenuIds[i],
+          container = $(link).attr("href"),
+          containerOffset = $(container).offset().top,
+          containerHeight = $(container).outerHeight(),
+          containerBottom = containerOffset + containerHeight;
+
+        if (
+          scrollPosition < containerBottom - 20 &&
+          scrollPosition >= containerOffset - 20
+        ) {
+          $(link).addClass("active");
+        } else {
+          $(link).removeClass("active");
+        }
+      });
+
+      // Aggiornare l'attributo href del link dinamico in base all'ultimo link attivo
+      var activeLink = $("a.nav-link.active");
+      var activeLinkId = activeLink.attr("href");
+      var currentPath = window.location.pathname;
+      var newPath = "";
+
+      if (currentPath.includes("/en/")) {
+        newPath = currentPath.replace("/en/", "/it/");
+      } else if (currentPath.includes("/it/")) {
+        newPath = currentPath.replace("/it/", "/en/");
+      }
+
+      var dynamicLinkHref = newPath + activeLinkId;
+      $("#dynamicLink").attr("href", dynamicLinkHref);
+    }
+  };
+
   util.scrollMenuIds = $("a.nav-link[href]");
   $("#menu").click(util.mobileMenu);
   $(window).resize(util.windowResize);
   $(document).scroll(util.scrollEvent);
+  $(document).ready(util.scrollEvent);
 });
---
+++ test/offcarr_demo/__settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1768682.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 137651574, 137651575, 137651576, 137651577, 137651916, 137653412, 137653414 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
